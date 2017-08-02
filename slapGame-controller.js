78d28bf3e16da60d879318e48d6a177bdf769084
Dee Foster <dee3724f@gmail.com>
2017-08-01 21:30:28 -0600

    final tweaks to slapgame

--- slapGame-controller.js
+++ slapGame-controller.js
@@ -3,58 +3,88 @@ function GameController() {
 
     update();
 
-    this.reset = function(){
+    this.reset = function () {
         reset();
     }
+
     this.attack = function attack(type) {
         gameService.attack(type);
-        let targt = gameService.getTarget();
+        
+        let health = gameService.getHealth();
+        if(health == 0.0){
+            return;
+        }
 
+        setTimeout(btnDisable,100);
 
+        document.getElementById('sub').src = '/effects/subattack/torp.gif';
+        setTimeout(function () { document.getElementById('sub').src = '/images/sub.jpg' }, 3000);
 
+        barGraph();
 
+        if (health > 0) {
+            setTimeout(function () { document.getElementById('ship').src = '/effects/shipattack/shipdamage.gif' }, 3000);
+            setTimeout(function () { document.getElementById('ship').src = './images/destroyer.jpg' }, 8000);
+            setTimeout(btnEnable, 8000);
+        }
 
+        if (health <= 0 && health > -100) {
+            setTimeout(function () { document.getElementById('ship').src = '/effects/shipdeath/ship explode.gif' }, 5000);
+            setTimeout(function () { document.getElementById('ship').src = '' }, 14000);
+            setTimeout(btnEnable, 14000);
+        }
+        if (health < -100) {
+            setTimeout(function () { document.getElementById('ship').src = '/effects/annihilate/nuke.gif' }, 5000);
+            setTimeout(function () { document.getElementById('ship').src = '' }, 8500);
+            setTimeout(btnEnable, 8000);
+        }
 
-
+        gameService.healthCheck();
 
         update();
     }
+
     this.giveItems = function giveItems(type) {
         gameService.giveItems(type);
     }
 
-
-
     function barGraph() {
         var targt = gameService.getTarget();
-
-        var bar = document.getElementById("vertBar"); 
-        var requiredHeight = (130 * targt.health.toFixed(0))/100
-        bar.style.height = requiredHeight + 'px'//requiredHeight;
-        
-        // console.log(targt.health.toFixed(0));
-        // console.log(bar.style.height = requiredHeight);
-       
+        var bar = document.getElementById("vertBar");
+        var requiredHeight = (130 * targt.health.toFixed(0)) / 100
+        bar.style.height = requiredHeight + 'px'
     }
 
     function update() {
         let targt = gameService.getTarget();
-        console.log(targt);
-        document.getElementById("health").innerText = targt.health.toFixed(0);
-        document.getElementById("hits").innerText = targt.hits;
-        document.getElementById("name").innerText = targt.name;
+
+        document.getElementById('health').innerText = targt.health.toFixed(1);
+        document.getElementById('hits').innerText = targt.hits;
+        document.getElementById('name').innerText = targt.name;
 
         barGraph();
     }
 
-    function reset(){
+    function reset() {
         gameService.reset();
         let targt = gameService.getTarget();
-        document.getElementById("health").innerText = targt.health.toFixed(0);
-        document.getElementById("hits").innerText = targt.hits;
+
+        document.getElementById('health').innerText = targt.health.toFixed(1);
+        document.getElementById('hits').innerText = targt.hits;
         document.getElementById('ship').src = './images/destroyer.jpg';
 
-        barGraph(); 
+        barGraph();
+    }
+
+    function btnDisable(){
+        document.getElementById('remote1').disabled = true;
+        document.getElementById('remote2').disabled = true;
+        document.getElementById('remote3').disabled = true;
+    }
+    function btnEnable(){
+        document.getElementById('remote1').disabled = false;
+        document.getElementById('remote2').disabled = false;
+        document.getElementById('remote3').disabled = false;
     }
 }
 
--- slapGame-service.js
+++ slapGame-service.js
@@ -7,6 +7,7 @@ function GameService() {
             this.items = [],
             this.addMods = function () {
                 var ttl = 0;
+
                 if (this.items.length < 1) {
                     return 1;
                 }
@@ -30,78 +31,29 @@ function GameService() {
     var items = {
         shield: new Item("Shield", 0.3, "This is an awesome shield!"),
         ADCAP: new Item("ADCAP", 1.5, "This is an awesome ADvanced CAPability torpedo!"),
-        nuke: new Item("Nuke", 25.0, "'Nuf said!")
+        nuke: new Item("Nuke", 15.0, "'Nuf said!")
     }
 
-    function barGraph() {
-        // var bar = document.getElementById("vertBar"); 
-        // var requiredHeight = (130 * targt.health.toFixed(0))/100;
-        // bar.style.height = requiredHeight;
-        
-        // console.log(targt.health.toFixed(0));
-        // console.log(bar.style.height = requiredHeight);
-       
-    }
+    var targt = new Target("Target", 100);
 
     function attackTarget(type) {
-        if(targt.health < 1){
+        if(targt.health == 0){
             alert('You\'re kidding right!? They\'re already Dead!' + '\r\nYou\'re a MONSTER!');
             return;
         }
 
-        // var damage = targt.attacks[type];
-        // var mods = targt.addMods().toFixed(1);
+        var damage = (Math.floor((Math.random() * targt.attacks[type]) + 1));
 
-
-
-        // document.getElementById('sub').src = '/effects/subattack/torp.gif';
-        // setTimeout(function(){document.getElementById('sub').src = '/images/sub.jpg'},3000);
-
-
-
-        var damage = targt.attacks[type];
         var mods = targt.addMods().toFixed(1);
-
         var temp = getAlertSummary(damage,mods);
         alert(temp);
 
         targt.health -= damage * mods;
-        targt.hits += 1;
-
-
-
-        document.getElementById('sub').src = '/effects/subattack/torp.gif';
-        setTimeout(function(){document.getElementById('sub').src = '/images/sub.jpg'},3000);
-
-        barGraph();
-
-        if(targt.health>0){
-            setTimeout(function(){document.getElementById('ship').src = '/effects/shipattack/shipdamage.gif'}, 3000);
-            setTimeout(function(){document.getElementById('ship').src = './images/destroyer.jpg'},8000);
-        }
-
-        if(targt.health<=0 && targt.health > -100){
-            setTimeout(function(){document.getElementById('ship').src = '/effects/shipdeath/ship explode.gif'}, 5000);
-            setTimeout(function(){document.getElementById('ship').src = ''},14000);
-        }
-        if(targt.health < -100){
-            setTimeout(function(){document.getElementById('ship').src = '/effects/annihilate/nuke.gif'}, 5000);
-            setTimeout(function(){document.getElementById('ship').src = ''},8500);
-        }
-
-
-
-        if (targt.health < 0) {
-            targt.health = 0;
-            alert('You annihilated your enemy!');
-        }
-
-
-        //runUpdate();
+        targt.hits += 1;  
     }
 
     function getAlertSummary(damage,mods){
-        var damageSummary = 'Expected damage is: ' + damage * mods;
+        var damageSummary = 'Expected damage is: ' + (damage * mods).toFixed(1);
         var itemSummary='Items Used: ';
 
         for(var i = 0; i < targt.items.length;i++){
@@ -112,10 +64,6 @@ function GameService() {
             }
         }
 
-        // if(itemSummary == 'Items Used: '){
-        //     itemSummary += 'none';
-        // }
-       
         var entire = damageSummary + "\r\n" + itemSummary;  
         return entire;
     }
@@ -130,45 +78,29 @@ function GameService() {
         }
     }
 
-     var targt = new Target("Target", 100);
-
-    // function runUpdate() {
-    //     document.getElementById("health").innerText = targt.health.toFixed(0);
-    //     document.getElementById("hits").innerText = targt.hits;
-    //     document.getElementById("name").innerText = targt.name;
-
-    //     barGraph();
-    // }
-
     function targetReset(){
         targt.health = 100;
         targt.hits = 0;
         targt.items.length = 0;
-        // document.getElementById("health").innerText = targt.health.toFixed(0);
-        // document.getElementById("hits").innerText = targt.hits;
-        // document.getElementById('ship').src = './images/destroyer.jpg';
-        // barGraph(); 
+    }
+
+    function healthCheckReset(){
+        if (targt.health < 0) {
+            targt.health = 0;
+            alert('You annihilated your enemy!');
+        }
     }
 
     //public
     this.attack = function attack(type){
-        attackTarget(type);
+        return attackTarget(type);
     }
 
     this.giveItems = function giveItems(type){
         giveTargetItems(type);
     }
 
-    // this.update = function update(){
-    //     runUpdate();
-    // }
-    
-    // this.reset = function reset(){
-    //     runReset();
-    // }
-
     this.getTarget = function getTarget(){
-        //var newTarget = targt;
         return targt;
     }
 
@@ -176,5 +108,11 @@ function GameService() {
         targetReset();
     }
 
+    this.getHealth = function getHealth(){
+        return targt.health;
+    }
 
+    this.healthCheck = function healthCheck(){
+        healthCheckReset();
+    }
 }
